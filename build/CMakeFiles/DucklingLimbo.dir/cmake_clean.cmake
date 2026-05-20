00405812800a7398b892a196ca101f3b3ab451a8
Lucy <lu.vg0603@gmail.com>
2026-05-20 00:35:43 -0500

    Creación de Cmake para compilar

---
+++ build/CMakeFiles/DucklingLimbo.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DucklingLimbo.dir/src/juego.cpp.o"
+  "CMakeFiles/DucklingLimbo.dir/src/juego.cpp.o.d"
+  "CMakeFiles/DucklingLimbo.dir/src/main.cpp.o"
+  "CMakeFiles/DucklingLimbo.dir/src/main.cpp.o.d"
+  "CMakeFiles/DucklingLimbo.dir/src/mundos.cpp.o"
+  "CMakeFiles/DucklingLimbo.dir/src/mundos.cpp.o.d"
+  "CMakeFiles/DucklingLimbo.dir/src/objeto.cpp.o"
+  "CMakeFiles/DucklingLimbo.dir/src/objeto.cpp.o.d"
+  "CMakeFiles/DucklingLimbo.dir/src/personaje.cpp.o"
+  "CMakeFiles/DucklingLimbo.dir/src/personaje.cpp.o.d"
+  "DucklingLimbo"
+  "DucklingLimbo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DucklingLimbo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
